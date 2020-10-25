8e0f95db70cc85ff7ccb25acdee902cbf87bab6b
Karl Estes <karlestes1@gmail.com>
2020-10-25 12:30:38 -0700

    Update of Milestone 1 intro comments

--- Portfolio Project/Milestone 1/shoppingCart.py
+++ Portfolio Project/Milestone 1/shoppingCart.py
@@ -5,13 +5,12 @@
 # This milestone program prompts the user to enter two item names, prices, and quantities of the items
 # It then outputs the information and the cost of the two items
 
-# I am using the Python extension for Visual Studio Code which includes the ability to create python cells 
-# which can be individually run in an interactive window. 
-# The syntax to create a cell is as follows: #%%
-# Any line with these three characters on it denotes the beginning of a 
-# python cell and is included due to the utilization of the python extension. 
+# I am using the Comment Anchors extension for Visual Studio Code which utilizes specific keywords
+# to allow for quick navigation around the file by creating sections and anchor points. Any use
+# of "ANCHOR", "TODO", "FIXME", "STUB", "NOTE", "REVIEW", "SECTION", and "LINK" are used in conjunction with 
+# this extension
 
-#%% Import Statements
+#Import Statements 
 
 
 #%% ItemToPurchase class
