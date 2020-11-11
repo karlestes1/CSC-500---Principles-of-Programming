7904724f36d2b284ea805901ef80e73805cb79c5
Karl Estes <karlestes1@gmail.com>
2020-11-10 20:06:48 -0800

    Module 6 File Handling
    - Ability to read in csv files
    - All files are loaded on program start
    - Error handling for load implemented
    - Welcome Message now printed

--- Module 6/courseInfo.py
+++ Module 6/courseInfo.py
@@ -15,12 +15,14 @@
 
 # SECTION - Imports and Global Variables
 from os import system, name
+import csv
 
-allCourses = [] # Will contain a list of all available courses sorted by course title
+allCourses = set([]) # Will contain a list of all available courses sorted by course title
 
 RESET = "\u001b[0m"
-WARNING = "u\001b[38;5;11m"
-ERROR = "u\001b[38;5;196m"
+WARNING = "\u001b[38;5;220m"
+ERROR = "\u001b[38;5;196m"
+INFO = "\u001b[38;5;123m"
 BOLD = "\u001b[38;5;15m"
 
 
@@ -28,7 +30,6 @@ BOLD = "\u001b[38;5;15m"
 
 # SECTION - File Handling
 
-# TODO - outline and complete function
 def readInCSV(filePath):
     '''
     Reads in the specified file with the assumption it follows a csv format
@@ -42,6 +43,39 @@ def readInCSV(filePath):
     Also adds courses to the global course list (TODO - create helper function to add things to the list w/o duplicates)
     '''
 
+    entryDict = {}
+
+    # Read in the CSV
+    with open(filePath) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 1
+
+        for row in csv_reader:
+            if line_count == 1:
+                line_count += 1
+            else:
+                # Add to the dictionary
+                try:
+                    entryDict[row[0]] = row[1]
+                    allCourses.add(row[0]) 
+                except (RuntimeError, TypeError, NameError, IndexError):
+                    printError(f">> Error reading in line {line_count} from {filePath}")
+                line_count += 1
+
+    return entryDict
+
+def loadAllFiles():
+    '''
+    Loads the three files required for the program (instructorInfo.csv, meetingTime.csv, and
+    roomInfo.csv) into dictionaries
+
+    Returns all three dictionaries in the following order: roomInfo, instructorInfo, meetingTime
+    '''
+    roomInfo = readInCSV("roomInfo.csv")
+    instructorInfo = readInCSV("instructorInfo.csv")
+    meetingTime = readInCSV("meetingTime.csv")
+
+    return roomInfo, instructorInfo, meetingTime
 
 # !SECTION
 
@@ -70,14 +104,46 @@ def displayCourseDetails(courseName, roomDict, instructorDict, meetingDict):
 # SECTION - Extra/Helper Functions
 
 # TODO - figure out what print functions for warning, error, title, etc are needed and write them
+def printWarning(stringToPrint):
+    print(WARNING + str(stringToPrint) + RESET)
 
-# TODO - write loader function to load all files on program load
+def printError(stringToPrint):
+    print(ERROR + str(stringToPrint) + RESET)
 
-# TODO - write welcome function
+def printInfo(stringToPrint):
+    print(INFO + str(stringToPrint) + RESET)
+
 def welcomeMessage():
     '''
     Prints a welcome message and clears the console on program start
     '''
+    # For windows
+    if name == 'nt':
+        _ = system('cls')
+    # Mac and linux 
+    else:
+        _ = system('clear')
+
+    # Font is Stick Letters from the Text to ASCII Art Generator (TAAG) on www.patorjk.com
+    print()
+    print(WARNING + r" __   __        __   __   ___            ___  __  ")
+    print(r"/  ` /  \ |  | |__) /__` |__     | |\ | |__  /  \ ")
+    print(r"\__, \__/ \__/ |  \ .__/ |___    | | \| |    \__/ " + RESET)
+    print("\nDeveloped by Karl Estes")
+    print("Created as per Module 6 Option 2 instructions in CSC 500 at CSUG\n\n")
+    printInfo("You may type q or quit at any point to exit this program")
+    printInfo("You may type ? or help at any point to see a list of commands")
+    print(BOLD + "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n" + RESET)
+
+def printMenu():
+    '''
+    Displays the meny of commands to the screen
+    '''
+    print(BOLD + "\n* * * Commands * * *" + RESET)
+    printInfo("'v'/'view' : View all courses")
+    printInfo("'i'/'info' : Will prompt for a course number to pull information for a specific course")
+    printInfo("'q'/'quit' : Quit the program")
+    printInfo("'?'/'help' : Print the commands menu\n")
 
 
 
@@ -89,11 +155,14 @@ def welcomeMessage():
 
 if __name__ == "__main__":
 
-    # TODO - call welcome message function
+    welcomeMessage()
+    printInfo(">> Loading course data...")
+    loadAllFiles()
+    printInfo(">> Course data has been loaded")
 
     # Loop with user input until program is quit
     while True:
-        print(">> ", end='') # TODO - Make arrows bold
+        print(BOLD + ">> " + RESET, end='') # TODO - Make arrows bold
         userInput = input()
 
         '''
@@ -109,15 +178,14 @@ if __name__ == "__main__":
             viewCourses()
         elif userInput == "i" or userInput == "info":
             # TODO - prompt for coursename and search
-            print("Need to implement")
+            printWarning("Need to implement")
         elif userInput == "?" or userInput == "help":
-            # TODO - print menu
-            print("Need to implement")
+            printMenu()
         elif userInput == "q" or userInput == "quit":
             exit(0)
         else:
             # TODO - print warning message (make warning function first)
-            print("Need to implement")
+            printWarning(">> Invalid Input - Please try again")
 
 
 
